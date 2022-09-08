d078daa22d3aa8a2addc7cd4abd3f152049b64ec
MochamadFarhanAbdullah <112877057+MochamadFarhanAbdullah@users.noreply.github.com>
2022-09-08 19:54:09 +0700

    Pertemuan-1

---
+++ dasproo/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ //iki tugas
+    cout << "Mochamad Farhan Abdullah" << endl;
+    cout << "A11.2022.14145" << endl;
+    cout << "Semarang" << endl;
+    cout << "SMAN 7 Semarang" << endl;
+    return 0;
+}
