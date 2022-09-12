afd9816e4f129a330ffbf1d18b082da6748f1f5a
MochamadFarhanAbdullah <112877057+MochamadFarhanAbdullah@users.noreply.github.com>
2022-09-12 15:37:17 +0700

    Pertemuan3

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    /*
+    Nama        : Mochamad Farhan Abdullah
+    Nim         : A11.2022.14145
+    Kelompok    : A11.4104
+    */
+    int u = 18;
+    float t = 165.1; //kayaknya
+    char b = 'M';
+    string ub = "XL";
+
+    cout << "ASHIAP!" << endl;
+    cout << "Umur saya= " << u;
+    cout << "\nTinggi badan= " << t;
+    cout << endl; //tes end line
+    cout << "Ukuran baju= " << b;
+    cout << endl;
+    cout << "Ukuran baju orang lain= " << ub;
+
+    //printf ("\nHalo dunia!");
+
+    return 0;
+}
